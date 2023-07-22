862c8e5968950bf091a48241678530640d54bebd
Edward <75829088+edwardgazitonline@users.noreply.github.com>
2023-07-22 13:37:37 +0300

    br-3
    Generating Tasks component
    inside Components folder

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,23 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+<<<<<<< Updated upstream
 
 @NgModule({
   declarations: [
     AppComponent
+=======
+import { HeaderComponent } from './components/header/header.component';
+import { ButtonComponent } from './components/button/button.component';
+import { TasksComponent } from './components/tasks/tasks.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    ButtonComponent,
+    TasksComponent
+>>>>>>> Stashed changes
   ],
   imports: [
     BrowserModule,
