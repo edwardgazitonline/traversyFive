19da4224c8f9078a7560d8600baeb93eac5b27b4
Edward <75829088+edwardgazitonline@users.noreply.github.com>
2023-07-28 21:46:13 +0300

    creating task-item
    first iteration

--- src/app/components/tasks/tasks.component.ts
+++ src/app/components/tasks/tasks.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 import { TASKS } from '../../mock-tasks';
 import { Task } from '../../Task';
