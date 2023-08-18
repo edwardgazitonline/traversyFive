f170fb3d8c63ce58f1449eb651c33dc41a22aa0c
Edward <75829088+edwardgazitonline@users.noreply.github.com>
2023-08-19 00:22:06 +0300

    br-10
    Creating the local server
    A special way to use and serve the data locally

---
+++ src/app/servises/task.service.ts
@@ -0,0 +1,21 @@
+import { Observable, of } from 'rxjs';
+
+import { Injectable } from '@angular/core';
+import { TASKS } from '../mock-tasks';
+import { Task } from '../Task';
+
+@Injectable({
+  providedIn: 'root',
+})
+
+
+export class TaskService {
+
+
+  constructor(){ }
+
+  getTasks(): Observable<Task[]>{
+    const tasks = of(TASKS);
+    return tasks;
+  }
+}
